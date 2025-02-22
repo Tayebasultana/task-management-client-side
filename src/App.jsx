3e2c85e512fe907f15a866c3749d9fb9a63e5ae9
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-02-22 23:06:56 +0600

    setup is done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-green-700 underline">
+      Hello world!
+    </h1>
+    <button className="btn btn-outline">Default</button>
+    <button className="btn btn-outline btn-primary">Primary</button>
+    <button className="btn btn-outline btn-secondary">Secondary</button>
+    <button className="btn btn-outline btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
